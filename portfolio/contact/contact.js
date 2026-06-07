e84fbe8eec6e05192828ebc337e57b886835e637
refdiu <lahirishraman@gmail.com>
2026-06-07 09:46:52 +0530

    scripted modals for project desc

--- portfolio/contact/contact.js
+++
@@ -1,8 +0,0 @@
-const sound = document.getElementById("aud");
-
-document.querySelectorAll(".butt").forEach(btn => {
-    btn.addEventListener("mouseenter", () => {
-        sound.currentTime = 0;
-        sound.play();
-    });
-});
